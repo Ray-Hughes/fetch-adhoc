e1a6ba5733b83728452b2867d71b76e129b9395e
Raymond Hughes <raymond.hughes@carepaths.com>
2023-02-18 20:52:26 -0500

    Refactor and finish. All tests passing per spec.

--- api/managed-records.js
+++ api/managed-records.js
@@ -3,35 +3,10 @@
 // /records endpoint
 window.path = "http://localhost:3000/records";
 
-function retrieve(options = {}) {
+async function retrieve(options = {}) {
   options.page = options.page || 1;
+  var page = options.page;
 
-  // peek is to check next page for data
-  var peek = 1;
-  var limit = 10;
-  options.limit = limit + peek;
-
-  if (!options.colors) options.colors = [];
-  if (!options["color[]"]) options["color[]"] = [];
-  options.colors.forEach(function(color) {
-    options["color[]"].push(color);
-  });
-
-  options.offset = (options.page - 1) * limit;
-  options.color = options.colors;
-
-  var uri = URI(window.path).search(options);
-
-  return fetch(uri)
-    .then(res => res.ok ? res.json() : Promise.reject(res.json()))
-    .then(payload => setPayload(payload, options))
-    .catch((err) => new Error(err));
-}
-
-function setPayload(payload, options = {}) {
-  var primaryColors = ['red', 'blue', 'yellow'];
-
-  // set required return object
   var data = {
     ids: [],
     open: [],
@@ -40,17 +15,54 @@ function setPayload(payload, options = {}) {
     nextPage: null
   }
 
-  if (options.page > 1) data.previousPage = options.page - 1;
+  // peek is to check next page for data
+  var peek = 1;
+  var limit = 10;
+  options.limit = limit + peek;
+
+  options.offset = (options.page - 1) * limit;
+  options.color = options.colors;
+
+  // default to empty array if nothing passed in options
+  if (!options.colors) options.colors = [];
+
+  // API looks for queryParam as "colors[]"
+  // Transform call from "colors" => "colors[]"
+  if (!options["color[]"]) options["color[]"] = [];
+  options.colors.forEach(function(color) {
+    options["color[]"].push(color);
+  });
+
+  var uri = URI(window.path).search(options);
+
+  // fetch returns a Promise
+  return fetch(uri)
+    .then(res => res.ok ? res.json() : Promise.reject(res.json()))
+    .then(payload => setPayload(data, payload, page))
+    .catch((err) => console.log(err));
+}
+
+function setPayload(data, payload, page) {
+  setPage(data, payload, page);
+  setDisposition(data, payload);
+  return data;
+}
+
+function setPage(data, payload, page) {
+  if (page > 1) data.previousPage = page - 1;
 
   if (payload.length > 10) {
-    data.nextPage = options.page + 1;
+    data.nextPage = page + 1;
     // don't return the peek page
     payload.pop();
   } else {
     data.nextPage = null;
   }
+}
 
-  // main loop to set data
+function setDisposition(data, payload) {
+  var primaryColors = ['red', 'blue', 'yellow'];
+
   payload.forEach((p) => {
     data.ids.push(p.id);
 
@@ -62,8 +74,6 @@ function setPayload(payload, options = {}) {
       data.closedPrimaryCount++;
     }
   });
-
-  return data;
 }
 
 export default retrieve;
